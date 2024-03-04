5a1f47908ed53a3a0d6c2a8606ba6f648c3e5850
dunggree <160002145+dunggree@users.noreply.github.com>
2024-03-04 16:29:37 +0900

    Update main.js

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
   var roomBackground = document.getElementById('room-background');
     // 마우스 포인터 위치 가지고 와서 원 위치 지정
   document.addEventListener('mousemove', function (e) {
-    roomBackground.style.clipPath = `circle(300px at ${e.clientX - 290}px ${e.clientY + 50}px)`;
+    roomBackground.style.clipPath = `circle(300px at ${e.clientX - 300}px ${e.clientY + 40}px)`;
   });
 });
 
